c52b4394894277103953bc4e2e957871da42a26f
Manoj <114227142+Manoj1408@users.noreply.github.com>
2023-03-31 13:38:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-//your JS code here. If required.
+var nameEle = document.querySelector("#name");
+var ageEle = document.querySelector("#age");
+var btnEle = document.querySelector("#btn");
+
+var promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        var age = ageEle.value;
+        var name = nameEle.value;
+        if(!(age && name)){
+            alert("Please enter valid details");
+            
+        }
+        return;
+        if(age < 18){
+            reject(`Oh sorry ${name}. You aren't old enough `);
+        }
+        else {
+            resolve(`Welcome,${name}. You can vote`);
+        }
+    }, 4000);
+
+});
+
+function callPromise(){
+    return promise;
+}
+
+function handlSubmit(e){
+    e.preventDefault();
+    callPromise().then(alert).catch(alert);
+}
+
+btnEle.addEventListener("click", handlSubmit);
